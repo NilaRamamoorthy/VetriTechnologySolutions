9544a7436a33084b2ec336640ad396e8eef2b0e9
NilaRamamoorthy <nilasibiraj@gmail.com>
2026-02-24 10:15:19 +0530

    FirstCommit

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hgass+-n-)3c+-%)j39_h0by5-(9s(2$hro$2ybrd!^0ew8i=^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
